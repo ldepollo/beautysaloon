f4cfc254221d17f8132ebafd6f07b3f6fc4e3834
Lucas Depollo <ldepollo@gmail.com>
2021-08-27 22:07:02 -0300

    inital commit

---
+++ main.js
@@ -0,0 +1,116 @@
+// toggle de menu quando clicar no ícone: hamburger e X
+
+const nav = document.querySelector('#header nav');
+const toggle = document.querySelectorAll('nav .toggle');
+
+for (const element of toggle) {
+  element.addEventListener('click', function () {
+    nav.classList.toggle('show');
+  })
+}
+
+// quando clicar em um item do menu, esconder o menu
+const links = document.querySelectorAll('nav ul li a');
+
+for (const link of links) {
+  link.addEventListener('click', function () {
+    nav.classList.remove('show');
+  })
+}
+
+// mudar o header da página quando der scroll
+const header = document.querySelector('#header');
+const navHeight = header.offsetHeight;
+
+
+function changeHeaderWhenScroll() {
+
+  if (window.scrollY >= navHeight) {
+    header.classList.add('scroll');
+  } else {
+    header.classList.remove('scroll');
+  }
+}
+
+// testimonials carousel slider swiper
+const swiper = new Swiper('.swiper-container', {
+  slidesPerView: 1,
+  pagination: {
+    el: '.swiper-pagination'
+  },
+  mousewheel: false,
+  keyboard: true,
+  breakpoints: {
+    767: {
+      slidesPerView: 2,
+      setWrapperSize: true
+    }
+  }
+})
+
+// scrollreveal: Mostrar elementos quando der scroll na pág.
+const scrollReveal = ScrollReveal({
+  origin: 'top',
+  distance: '30px',
+  duration: 700,
+  reset: true
+})
+
+scrollReveal.reveal(`
+#home .image, #home .text,
+#about .image, #about .text,
+#services header, #services .card,
+#testimonials header, #testimonials .testimonials,
+#contact .text, #contact .links,
+#footer .brand, #footer .social
+`, { interval: 100 });
+
+
+// botão "back to top"
+const backToTopButton = document.querySelector('.back-to-top');
+
+function backToTop() {
+  if (window.scrollY >= 560) {
+    backToTopButton.classList.add('show');
+  } else {
+    backToTopButton.classList.remove('show');
+  }
+}
+
+// menu ativo conforme a seção visível na página
+const sections = document.querySelectorAll('main section[id]')
+function activateMenuAtCurrentSection() {
+  const checkpoint = window.pageYOffset + (window.innerHeight / 8) * 4
+
+  for (const section of sections) {
+    const sectionTop = section.offsetTop
+    const sectionHeight = section.offsetHeight
+    const sectionId = section.getAttribute('id')
+
+    const checkpointStart = checkpoint >= sectionTop
+    const checkpointEnd = checkpoint <= sectionTop + sectionHeight
+
+    if (checkpointStart && checkpointEnd) {
+      document
+        .querySelector('nav ul li a[href*=' + sectionId + ']')
+        .classList.add('active')
+    } else {
+      document
+        .querySelector('nav ul li a[href*=' + sectionId + ']')
+        .classList.remove('active')
+    }
+  }
+}
+
+
+//when scroll
+
+window.addEventListener('scroll', function () {
+  changeHeaderWhenScroll()
+  backToTop()
+  activateMenuAtCurrentSection()
+})
+
+
+
+
---
+++ teste.js
@@ -0,0 +1,4 @@
+const visivel = true;
+const teste = "teste"
+
+console.log(visivel || teste);
